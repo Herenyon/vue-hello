602958dad072e43993bc6572ca6456f9b9964c7e
Herenyon <rikitur17@gmail.com>
2023-03-13 16:57:53 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const { createApp } = Vue
+
+    createApp({
+        data(){
+            return {
+             titoloBig: 'Questo è il messaggio'
+            }
+        }
+    }).mount('#titolo');
+
+    const titoloBig = 'Messaggione Titolone';
